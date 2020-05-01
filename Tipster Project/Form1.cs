273bd406bb6b4473be2d8d046d4f4f9eb4098e05
Dylan Hamilton <dylan5437@hotmail.co.uk>
2020-05-01 09:33:35 +0100

    Fixed code

--- Tipster Project/Form1.cs
+++ Tipster Project/Form1.cs
@@ -27,23 +27,64 @@ namespace Tipster_Project
         /// This calculates how much people will need to pay,
         /// passes all of the information onto the results
         /// form and then displays all of the needed information
-        /// for the user, but will catch any errors on the data
-        /// entry part.
+        /// for the user.
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void calculateCosts(object sender, EventArgs e)
         {
             resultsForm newForm = new resultsForm();
 
+            answer = tip / Convert.ToDouble(peopleListBox.SelectedItem);
+            newForm.Show();
+            newForm.billLabel.Text = billTextBox.Text;
+            newForm.peopleLabel.Text = peopleListBox.SelectedItem.ToString();
+            newForm.tipTotalLabel.Text = tip.ToString("0.00");
+            newForm.totalLabel.Text = "£" + answer.ToString("0.00");
+        }
+
+        /// <summary>
+        /// This makes sure that the bill amount is entered
+        /// in the form first and then adds the tip onto
+        /// the bill amount, this will also catch any errors on the data
+        /// entry part.
+        /// </summary>
+        private void addNoTip(object sender, EventArgs e)
+        {
             try
             {
-                answer = tip / Convert.ToDouble(peopleListBox.SelectedItem);
-                newForm.Show();
-                newForm.billLabel.Text = billTextBox.Text;
-                newForm.peopleLabel.Text = peopleListBox.SelectedItem.ToString();
-                newForm.tipTotalLabel.Text = tip.ToString("0.00");
-                newForm.totalLabel.Text = "£" + answer.ToString("0.00");
+                if (billTextBox.Text == "")
+                {
+                    MessageBox.Show("Please enter the bill amount before selecting a tip.");
+                }
+                else
+                {
+                    tip = Convert.ToDouble(billTextBox.Text) * no;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error in Data Entry");
+            }
+            
+        }
+
+        /// <summary>
+        /// This makes sure that the bill amount is entered
+        /// in the form first and then adds the tip onto
+        /// the bill amount, this will also catch any errors on the data
+        /// entry part.
+        /// </summary>
+        private void addNormalTip(object sender, EventArgs e)
+        {
+            try
+            {
+                if (billTextBox.Text == "")
+                {
+                    MessageBox.Show("Please enter the bill amount before selecting a tip.");
+                }
+                else
+                {
+                    tip = Convert.ToDouble(billTextBox.Text) * normal;
+                }
             }
             catch
             {
@@ -54,68 +95,50 @@ namespace Tipster_Project
         /// <summary>
         /// This makes sure that the bill amount is entered
         /// in the form first and then adds the tip onto
-        /// the bill amount
-        /// </summary>
-        private void addNoTip(object sender, EventArgs e)
-        {
-            if (billTextBox.Text == "")
-            {
-                MessageBox.Show("Please enter the bill amount before selecting a tip.");
-            }
-            else
-            {
-                tip = Convert.ToDouble(billTextBox.Text) * no;
-            }
-        }
-
-        /// <summary>
-        /// This makes sure that the bill amount is entered
-        /// in the form first and then adds the tip onto
-        /// the bill amount
-        /// </summary>
-        private void addNormalTip(object sender, EventArgs e)
-        {
-            if (billTextBox.Text == "")
-            {
-                MessageBox.Show("Please enter the bill amount before selecting a tip.");
-            }
-            else
-            {
-                tip = Convert.ToDouble(billTextBox.Text) * normal;
-            }
-        }
-
-        /// <summary>
-        /// This makes sure that the bill amount is entered
-        /// in the form first and then adds the tip onto
-        /// the bill amount
+        /// the bill amount, this will also catch any errors on the data
+        /// entry part.
         /// </summary>
         private void addGenerousTip(object sender, EventArgs e)
         {
-            if (billTextBox.Text == "")
+            try
             {
-                MessageBox.Show("Please enter the bill amount before selecting a tip.");
+                if (billTextBox.Text == "")
+                {
+                    MessageBox.Show("Please enter the bill amount before selecting a tip.");
+                }
+                else
+                {
+                    tip = Convert.ToDouble(billTextBox.Text) * generous;
+                }
             }
-            else
+            catch
             {
-                tip = Convert.ToDouble(billTextBox.Text) * generous;
+                MessageBox.Show("Error in Data Entry");
             }
         }
 
         /// <summary>
         /// This makes sure that the bill amount is entered
         /// in the form first and then adds the tip onto
-        /// the bill amount
+        /// the bill amount, this will also catch any errors on the data
+        /// entry part.
         /// </summary>
         private void addMadTip(object sender, EventArgs e)
         {
-            if(billTextBox.Text == "")
+            try
             {
-                MessageBox.Show("Please enter the bill amount before selecting a tip.");
+                if (billTextBox.Text == "")
+                {
+                    MessageBox.Show("Please enter the bill amount before selecting a tip.");
+                }
+                else
+                {
+                    tip = Convert.ToDouble(billTextBox.Text) * mad;
+                }
             }
-            else
+            catch
             {
-                tip = Convert.ToDouble(billTextBox.Text) * mad;
+                MessageBox.Show("Error in Data Entry");
             }
         }
 
